2b8acc475f76054291c8812001981ab3e6bb3606
danubiobwm <danubio.bwm@gmail.com>
2024-01-06 23:20:08 -0300

    time zone

--- src/main/java/com/parkingapi/demoparkingapi/config/SpringTimezoneConfig.java
+++ src/main/java/com/parkingapi/demoparkingapi/config/SpringTimezoneConfig.java
@@ -1,13 +1,14 @@
 package com.parkingapi.demoparkingapi.config;
 
+import jakarta.annotation.PostConstruct;
 import org.springframework.context.annotation.Configuration;
 
 import java.util.TimeZone;
 
 @Configuration
 public class SpringTimezoneConfig {
-  public void SpringTimezoneConfig() {
-
+  @PostConstruct
+  public void TimezoneConfig() {
     TimeZone.setDefault(TimeZone.getTimeZone("America/Fortaleza"));
   }
 }

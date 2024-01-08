9d8593d44ece5d06a0225ec1c6565380b2fa0278
danubiobwm <danubio.bwm@gmail.com>
2024-01-07 23:01:54 -0300

    all users
    Signed-off-by: danubiobwm <danubio.bwm@gmail.com>

--- src/main/java/com/parkingapi/demoparkingapi/service/UsuarioService.java
+++ src/main/java/com/parkingapi/demoparkingapi/service/UsuarioService.java
@@ -1,18 +1,37 @@
 package com.parkingapi.demoparkingapi.service;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.parkingapi.demoparkingapi.entity.Usuario;
 import com.parkingapi.demoparkingapi.repository.UsuarioRepository;
-import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @RequiredArgsConstructor
 @Service
 public class UsuarioService {
     private final UsuarioRepository usuarioRepository;
-
     @Transactional
     public Usuario salvar(Usuario usuario) {
         return usuarioRepository.save(usuario);
+    }
+
+    @Transactional(readOnly = true)
+    public Usuario buscarPorId(Long id) {
+        return usuarioRepository.findById(id).orElseThrow(
+                () -> new RuntimeException("Usuário não encontrado")
+        );
+    }
+    @Transactional
+    public Usuario editarSenha(Long id, String password) {
+       Usuario user = buscarPorId(id);
+       user.setPassword(password);
+       return user;
+    }
+
+    @Transactional(readOnly = true)
+    public List<Usuario> buscarTodos() {
+        return usuarioRepository.findAll();
     }
 }

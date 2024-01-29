d182d2f488693a82fdd001c8ea670aa6c294dd28
danubiobwm <danubio.bwm@gmail.com>
2024-01-28 21:25:46 -0300

    finish jwt
    Signed-off-by: danubiobwm <danubio.bwm@gmail.com>

--- src/main/java/com/parkingapi/demoparkingapi/entity/Usuario.java
+++ src/main/java/com/parkingapi/demoparkingapi/entity/Usuario.java
@@ -3,6 +3,11 @@ package com.parkingapi.demoparkingapi.entity;
 
 import jakarta.persistence.*;
 import lombok.*;
+import org.springframework.data.annotation.CreatedBy;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedBy;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 
 import java.io.Serializable;
@@ -12,6 +17,7 @@ import java.util.Objects;
 @Getter  @Setter @NoArgsConstructor
 @Entity
 @Table(name = "usuarios")
+@EntityListeners(AuditingEntityListener.class)
 public class Usuario implements Serializable {
 
     @Id
@@ -26,13 +32,17 @@ public class Usuario implements Serializable {
     @Column(name = "role", nullable = false, length = 25)
     private Role role = Role.ROLE_CLIENTE;
 
+
+    @CreatedDate
     @Column(name = "data_criacao")
     private LocalDateTime dataCriacao;
+    @LastModifiedDate
     @Column(name = "data_modificacao")
     private LocalDateTime dataModificacap;
-
+    @CreatedBy
     @Column(name = "criado_por")
     private String criadoPor;
+    @LastModifiedBy
     @Column(name = "modificado_por")
     private String modificadoPor;
 

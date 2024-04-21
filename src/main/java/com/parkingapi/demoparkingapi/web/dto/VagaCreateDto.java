745df226b7340829789f6d0dad2baad8c4054709
danubiobwm <danubio.bwm@gmail.com>
2024-04-21 18:33:15 -0300

    msg de internalização

--- src/main/java/com/parkingapi/demoparkingapi/web/dto/VagaCreateDto.java
+++ src/main/java/com/parkingapi/demoparkingapi/web/dto/VagaCreateDto.java
@@ -10,7 +10,7 @@ import lombok.Setter;
 
 @Getter @Setter @NoArgsConstructor @AllArgsConstructor
 public class VagaCreateDto {
-    @NotBlank
+    @NotBlank(message = "{NotBlank.vagaCreateDto.codigo}")
     @Size(min = 4, max = 4)
     private String codigo;
     @NotBlank
